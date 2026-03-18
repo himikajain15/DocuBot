6abf3570d0258cb87fe160294ea603b0517c9fa6
Himika Jain <himikajain2110@gmail.com>
2026-03-18 18:27:48 +0530

    Update remaining LangChain imports

--- DocuBot.py
+++ DocuBot.py
@@ -8,12 +8,12 @@ from bs4 import BeautifulSoup
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.documents import Document
-from langchain.prompts import ChatPromptTemplate
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
 from streamlit.errors import StreamlitSecretNotFoundError
 
 
